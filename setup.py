975aed5aad50c8d5c8e34fead6af25c7fd950314
Rem <128343390+import-pandas-as-numpy@users.noreply.github.com>
2023-04-10 23:13:49 -0400

    Added some stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='safepull',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='rem',
+    author_email='',
+    description='CLI tool for safe pulling of .py files from PyPI tarballs and wheels.'
+)
